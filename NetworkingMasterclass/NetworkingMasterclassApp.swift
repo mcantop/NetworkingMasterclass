43c41113043168ebdcca2b7596660429f2c9b542
mcantop <maciejcantop@icloud.com>
2023-11-18 14:43:19 +0100

    Initial Commit

---
+++ NetworkingMasterclass/NetworkingMasterclassApp.swift
@@ -0,0 +1,17 @@
+//
+//  NetworkingMasterclassApp.swift
+//  NetworkingMasterclass
+//
+//  Created by Maciej on 18/11/2023.
+//
+
+import SwiftUI
+
+@main
+struct NetworkingMasterclassApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
